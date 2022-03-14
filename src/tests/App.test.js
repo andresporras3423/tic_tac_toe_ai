ebc83e9711f1a3f7e78b55ac4aaa5b2b6efc4fc1
andresporras3423 <oscarrussi@outlook.com>
2022-03-13 23:18:16 -0500

    add test to count images with specific src

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render, within } from '@testing-library/react';
+import { render, fireEvent, prettyDOM } from '@testing-library/react';
 import App from '../components/App';
 
 test('check if page has a start text', () => {
@@ -15,12 +15,12 @@ test('check if page has text "play with O"', () => {
   const component = render(<App />);
   expect(component.getByText("Play with O")).toBeInTheDocument();
 });
-test('test radio button is checked', () => {
+test('test radio button to start with x is checked', () => {
   const component = render(<App />);
   const piece = component.getByTestId("radio-x");
   expect(piece).toBeChecked();
 });
-test('test radio button is checked', () => {
+test('test radio button to start with o is checked', () => {
   const component = render(<App />);
   const piece = component.getByTestId("radio-o");
   expect(piece).not.toBeChecked();
@@ -28,5 +28,12 @@ test('test radio button is checked', () => {
 test('check that div board has 9 images', () => {
   const component = render(<App />);
   const images = component.container.querySelectorAll('img');
+  expect(images.length).toBe(9);
+});
+test('check all source images are empty after player start a game with default configuration', () => {
+  const component = render(<App />);
+  const button = component.getByText(/start/i);
+  fireEvent.click(button);
+  const images = component.container.querySelectorAll('img[src=""]');
   expect(images.length).toBe(9);
 });
